4e661da7ea1e9006f6caaac6243be2bf18d9a0e5
Marco <ocri0808@gmail.com>
2025-09-01 12:53:13 +0200

    Fixed FuelItem "getBurnTime()" override functions params

--- src/main/java/vectorwing/farmersdelight/common/item/FuelBlockItem.java
+++ src/main/java/vectorwing/farmersdelight/common/item/FuelBlockItem.java
@@ -4,6 +4,7 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.FuelValues;
 
 import javax.annotation.Nullable;
 
@@ -22,7 +23,7 @@ public class FuelBlockItem extends BlockItem
 	}
 
 	@Override
-	public int getBurnTime(ItemStack stack, @Nullable RecipeType<?> recipeType) {
+	public int getBurnTime(ItemStack stack, @Nullable RecipeType<?> recipeType, FuelValues fuelValues) {
 		return this.burnTime;
 	}
 }
