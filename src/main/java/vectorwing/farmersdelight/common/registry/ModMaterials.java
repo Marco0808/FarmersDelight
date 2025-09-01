35e04b5c160d303980f78518c86f8b20b3a2a048
Marco <ocri0808@gmail.com>
2025-09-01 18:42:45 +0200

    Removed finished todo comment from "ModMaterials" class

--- src/main/java/vectorwing/farmersdelight/common/registry/ModMaterials.java
+++ src/main/java/vectorwing/farmersdelight/common/registry/ModMaterials.java
@@ -5,6 +5,5 @@ import net.minecraft.world.item.ToolMaterial;
 import vectorwing.farmersdelight.common.tag.ModTags;
 
 public class ModMaterials {
-	// TODO:MP: Add "flint_tool_materials" tag file and add "minecraft:flint" to it
 	public static final ToolMaterial FLINT = new ToolMaterial(BlockTags.INCORRECT_FOR_WOODEN_TOOL, 131, 4.0F, 1.0F, 5, ModTags.FLINT_TOOL_MATERIALS);
 }
